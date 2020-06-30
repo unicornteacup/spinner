d691b32b510b9564a606b5c81af394ccefc1f3c2
Caroline <carolinejepps@gmail.com>
2020-06-30 22:25:44 +0000

    Added spinner 1

---
+++ spinner1.js
@@ -0,0 +1,38 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 2000);
+
